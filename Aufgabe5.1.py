44d4ee1f5691809987c29b91df82a32ff5b31ebe
dangminh214 <dangminh214@gmail.com>
2023-06-28 02:29:05 +0200

    5.1 change

--- Aufgabe5.1.py
+++ Aufgabe5.1.py
@@ -1,6 +1,5 @@
 import cv2
 from matplotlib import pyplot as plt
-
 import numpy as np
 
 def plotHistogram(image, cumulative=False):
@@ -14,25 +13,30 @@ def plotHistogram(image, cumulative=False):
     plt.show()
     return hist
 
-img = cv2.imread('Aufgabe5_Material/schrott.png')
-scaled_img = np.clip(img, 100, 150)
+img = cv2.imread('Aufgabe5_Material/schrott.png', cv2.IMREAD_GRAYSCALE)
 
-# Ermitteln Sie den minimalen und maximalen Grauwert des skalierten Bildes
+
+# min and max contrast value of original image
+min_value = np.min(img)
+max_value = np.max(img)
+
+# scale to range 100 - 150
+scaled_img = ((img - min_value) / (max_value - min_value)) * 50 + 100
+
+# update min max values to scaled image instead of original image
 min_value = np.min(scaled_img)
 max_value = np.max(scaled_img)
 
-# Skalieren Sie das Bild auf den vollen Kontrastumfang [0...255]
+# scale all of them up to 255
 max_contrast_img = ((scaled_img - min_value) / (max_value - min_value)) * 255
 
-hist = plotHistogram(img, False)
-plotHistogram(scaled_img, False)
+plotHistogram(img, False)
+plotHistogram(scaled_img.astype(np.uint8), False)
+plotHistogram(max_contrast_img.astype(np.uint8), False)
 
 cv2.imshow("original image", img)
-cv2.imshow("max contrast image", max_contrast_img)
+cv2.imshow("scaled image", scaled_img.astype(np.uint8))
+cv2.imshow("max contrast image", max_contrast_img.astype(np.uint8))
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
-
